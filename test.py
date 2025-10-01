5fe104288d0c2cf43557a1e4bb40ce48ba334ce4
Zhizheng Fu <charles-frazier@outlook.com>
2025-10-01 15:42:02 +0800

    update

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import torch
 import lightning
+from lightning.pytorch.loggers import CSVLogger
 from src.engin import FSIR
 from src.config import Config
 from src.utils import init_seed
@@ -21,13 +22,16 @@ config = Config.from_toml(f"{CONFIG_NAME}.toml")
 init_seed(config.seed, config.deterministic)
 model = FSIR.load_from_checkpoint(checkpoint_path, config=config.trainer)
 datasets_config = config.datasets
-trainer = lightning.Trainer()
+trainer = lightning.Trainer(
+    logger=CSVLogger("logs_test", name=f"test_{TYPE}")
+)
 
 for sigma in SIGMA:
     for sf_idx, sf in enumerate(SF):
         for k_idx, kernel in enumerate(K):
             if TYPE == "bicubic" and k_idx != sf_idx:
                 continue
+            print(f"--- sigma:{sigma} sf:{sf} k_idx:{k_idx} ---")
             kernel = K[k_idx]
             datasets_config.test.sigma = sigma
             datasets_config.test.sf = sf
