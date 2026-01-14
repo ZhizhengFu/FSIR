576445c44f6f9503b2606e2bedd93173b87f1245
Zhizheng Fu <fuzz@Zhizhengs-MacBook-Pro.local>
2026-01-14 21:03:37 +0800

    fix

--- src/engin/trainer.py
+++ src/engin/trainer.py
@@ -65,15 +65,13 @@ class FSIR(L.LightningModule):
         mse_loss = self.mse(x, x_est)
         psnr_value = self.psnr(x, x_est)
         ssim_value = self.ssim(x, x_est)
-        batch_size = x.size(0)
 
-        self.log("test_mse_loss", mse_loss, batch_size=batch_size)
-        self.log("test_psnr", psnr_value, batch_size=batch_size)
-        self.log("test_ssim", ssim_value, batch_size=batch_size)
+        self.log("test_psnr", psnr_value, on_step=True)
+        self.log("test_ssim", ssim_value, on_step=True)
 
         if self.curr_sf != 0 and self.curr_sf == sf:
-            if batch_index == 0:
-                self.k_idx += 1
+            if batch_index==0:
+                self.k_idx +=1
         else:
             self.k_idx = 1
         self.curr_sf = sf
@@ -87,8 +85,8 @@ class FSIR(L.LightningModule):
         )
         save_dir.mkdir(parents=True, exist_ok=True)
 
-        save_image(x_est, save_dir / f"sr_{batch_index + 1}.png")
-        save_image(x, save_dir / f"hr_{batch_index + 1}.png")
+        save_image(x_est, save_dir / f"sr_{batch_index+1}.png")
+        save_image(x, save_dir / f"hr_{batch_index+1}.png")
 
         return mse_loss
 
